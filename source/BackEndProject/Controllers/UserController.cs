09c8b48ae16f10cb09d004acc5c130e10116f3f2
Alexis DUCHET <59805596+Ratcraft@users.noreply.github.com>
2021-04-29 14:38:05 +0100

    user ++

--- source/BackEndProject/Controllers/UserController.cs
+++ source/BackEndProject/Controllers/UserController.cs
@@ -1,26 +1,102 @@
-using Data;
-using Models;
-using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
-using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore;
-using System;
 using DTO;
+using Helpers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Models;
+using Services;
+using Data;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
-namespace BackEndProject.Controllers
+namespace Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("[controller]")]
     public class UserController : ControllerBase
     {
-        private readonly IMapper _mapper;
+        private IUserService _userService;
+        private IMapper _mapper;
+        public IConfiguration Configuration;
         private readonly Context _context;
-        public UserController(Context context, IMapper mapper)
+        private readonly IEmailService _emailService;
+
+        public UserController(
+            Context context,
+            IUserService userService,
+            IMapper mapper,
+            IConfiguration configuration,
+            IEmailService emailService)
         {
             _context = context;
+            _userService = userService;
             _mapper = mapper;
+           Configuration = configuration;
+           _emailService = emailService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate(AuthenticateModel model)
+        {
+            var user = _userService.Authenticate(model.Username, model.Password);
+
+            if (user == null)
+                return BadRequest(new { message = "Username or password is incorrect" });
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(Configuration["Secret"]);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.id.ToString()),
+                    new Claim(ClaimTypes.Role, user.levelAccess.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+
+            // return basic user info and authentication token
+            return Ok(new
+            {
+                Id = user.id,
+                Username = user.userName,
+                FirstName = user.firstName,
+                LastName = user.lastName,
+                Token = tokenString
+            });
+        }
+
+        [AllowAnonymous]
+        [HttpPost("register")]
+        public IActionResult Register([FromBody]CreateUser model)
+        {
+            // map model to entity
+            var user = _mapper.Map<User>(model);
+
+            try
+            {
+                // create user
+                _userService.Create(user, model.Password);
+                return Ok();
+            }
+            catch (AppException ex)
+            {
+                // return error message if there was an exception
+                return BadRequest(new { message = ex.Message });
+            }
         }
 
         [HttpGet]
