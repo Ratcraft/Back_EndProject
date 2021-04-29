09126e21f5ad680f8dccde51633e551067203bb6
SwLina <tony.fiori@epita.fr>
2021-04-29 14:24:41 +0100

    update applying, context, joboffer

--- source/BackEndProject/Models/Joboffer.cs
+++ source/BackEndProject/Models/Joboffer.cs
@@ -9,11 +9,19 @@ namespace Models
     {
         [Key]
         public int id { get; set; }
+
         public string name { get; set; }
+
         public string description { get; set; }
+
         public string duration { get; set; }
+
         public DateTime dateCreated { get; set; }
+
         public string bossusername { get; set; }
+
+        public DateTime deadline { get; set; }
+
         //public List<int> applying_people { get; set; }
         //public List<int> working_people { get; set; }
     }
