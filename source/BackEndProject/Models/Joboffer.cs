c39d8589b50be21c94b70c309420ee90866276ed
Alexis DUCHET <59805596+Ratcraft@users.noreply.github.com>
2021-04-29 15:50:38 +0100

    better startup

--- source/BackEndProject/Models/Joboffer.cs
+++ source/BackEndProject/Models/Joboffer.cs
@@ -9,17 +9,11 @@ namespace Models
     {
         [Key]
         public int id { get; set; }
-
         public string name { get; set; }
-
         public string description { get; set; }
-
         public string duration { get; set; }
-
         public DateTime dateCreated { get; set; }
-
         public string bossusername { get; set; }
-
         public DateTime deadline { get; set; }
 
         //public List<int> applying_people { get; set; }
--- source/BackEndProject/Startup.cs
+++ source/BackEndProject/Startup.cs
@@ -39,47 +39,7 @@ namespace BackEndProject
             });
             services.AddCors();
             services.AddControllers();
-
-            services.AddSwaggerGen(swagger =>
-            {
-                //This is to generate the Default UI of Swagger Documentation  
-                swagger.SwaggerDoc("v1", new OpenApiInfo
-                {
-                    Version = "v1",
-                    Title = "Dorset College API",
-                    Description = "ASP.NET Core 3.1 Web API Documentaion"
-                });
-                // To Enable authorization using Swagger (JWT)  
-                swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
-                {
-                    Name = "Authorization",
-                    Type = SecuritySchemeType.ApiKey,
-                    Scheme = "Bearer",
-                    BearerFormat = "JWT",
-                    In = ParameterLocation.Header,
-                    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
-                });
-                swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
-                {
-                    {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference
-                            {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = "Bearer"
-                            }
-                        },
-                        new string[] {}
-                    }
-                });
-            });
-
-            // configure DI for application services
-            services.AddScoped<IUserService, UserService>();
-
-            // Adding Sendgrid Email service
-            services.AddScoped<IEmailService, EmailService>();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
             // configure jwt authentication
             var key = Encoding.ASCII.GetBytes(Configuration["Secret"]);
@@ -115,6 +75,7 @@ namespace BackEndProject
                     ValidateAudience = false
                 };
             });
+
             // configure DI for application services
             services.AddScoped<IUserService, UserService>();
 
@@ -154,7 +115,7 @@ namespace BackEndProject
                         new string[] {}  
                     }  
                 });  
-            });
+            });  
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
